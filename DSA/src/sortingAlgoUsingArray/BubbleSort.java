9f07930398b287e602072814447921d88226af18
prajapati25 <prajapatihimanshu178@gmail.com>
2021-06-12 23:48:22 +0530

    Bubble Sorting using  array

--- DSA/src/sortingAlgoUsingArray/BubbleSort.java
+++ DSA/src/sortingAlgoUsingArray/BubbleSort.java
@@ -5,16 +5,15 @@ import java.util.Arrays;
 public class BubbleSort {
 
 	public static void main(String[] args) {
-		int[] arr = new int[] { 1, 2, 5, -2, 100, 10, -6, 1, 2 };
-		BubbleSort bs = new BubbleSort();
+		int[] arr = new int[] { 1, 2, 5, -2, 100, 10, -6 };
+		BubbleSort bs= new BubbleSort();
 		bs.bubbleSort(arr);
 	}
-
 	public void bubbleSort(int[] arr) {
-
+		
 		int n = arr.length;
 		while (n > 0) {
-			for (int i = 0; i < n - 1; i++) {
+			for (int i = 0; i < n-1; i++) {
 				if (arr[i] > arr[i + 1]) {
 
 					int b = arr[i];
@@ -24,7 +23,7 @@ public class BubbleSort {
 			}
 			n--;
 		}
-
+		
 		System.out.println(Arrays.toString(arr));
 	}
 }
