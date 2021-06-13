3554166473ea5fb2893e674a973da4aae68d5421
prajapati25 <prajapatihimanshu178@gmail.com>
2021-06-13 23:48:57 +0530

    Factorial of given number using recursion.

---
+++ DSA/src/exercises/FactorialUsingRecursion.java
@@ -0,0 +1,27 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class FactorialUsingRecursion {
+
+	private static Scanner scanner;
+
+	public static void main(String[] args) {
+		System.out.println("Enter a no.");
+		scanner = new Scanner(System.in);
+		int num = scanner.nextInt();
+		int fact = factorial(num);
+		System.out.format("factorial of  %d is %d" , num,fact);
+		
+
+	}
+
+	private static int factorial(int num) {
+		
+		// TODO Auto-generated method stub
+		if(num==0 ||num ==1)
+		return 1;
+		return num*factorial(num-1) ;
+	}
+
+}
